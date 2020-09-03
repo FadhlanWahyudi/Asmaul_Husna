fc6fa47fb488b478aeb837506276f146c5bc7f50
MuhammadFadhlanWahyudi <fadhlandeveloper@gmail.com>
2020-09-03 08:10:42 +0700

    :tada: Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:asmaul_husna/view/listItem.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.lightBlueAccent,
+        title: Text('Asmaul Husna'),
+      ),
+      body: myListItem(),
+    );
+  }
+}
+
+final baseTextStyle = const TextStyle(color: Colors.white, fontFamily: 'arial');
+final descTextStyle =
+    baseTextStyle.copyWith(fontSize: 15.0, fontWeight: FontWeight.w400);
+final footerTextStyle =
+    baseTextStyle.copyWith(fontSize: 12.0, fontWeight: FontWeight.w400);
